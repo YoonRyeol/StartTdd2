8a608df61e4bc12b9ddc9231566b99e3e4692439
YoonRyeol <yunyul3@gmail.com>
2022-06-20 15:38:21 +0900

    코드 정리: 상수를 변수로

--- src/main/kotlin/ExpiryDateCalculator.kt
+++ src/main/kotlin/ExpiryDateCalculator.kt
@@ -2,7 +2,8 @@ import java.time.LocalDate
 
 class ExpiryDateCalculator {
     fun calculateExpiryDate(payData: PayData): LocalDate {
-        val candidateExp = payData.billingDate!!.plusMonths(1)
+        val addendMonths = 1L
+        val candidateExp = payData.billingDate!!.plusMonths(addendMonths)
         if(payData.firstBillingDate!!.dayOfMonth != candidateExp.dayOfMonth)
             return candidateExp.withDayOfMonth(payData.firstBillingDate.dayOfMonth)
         return payData.billingDate!!.plusMonths(1)
