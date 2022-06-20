53b2fe5f26a23298cc4f8efa74d656614d731880
YoonRyeol <yunyul3@gmail.com>
2022-06-20 15:59:47 +0900

    예외 상황 테스트 추가

--- src/main/kotlin/ExpiryDateCalculator.kt
+++ src/main/kotlin/ExpiryDateCalculator.kt
@@ -1,11 +1,18 @@
 import java.time.LocalDate
+import java.time.YearMonth
 
 class ExpiryDateCalculator {
     fun calculateExpiryDate(payData: PayData): LocalDate {
         val addendMonths = payData.payAmount!!/10_000L
         val candidateExp = payData.billingDate!!.plusMonths(addendMonths)
-        if(payData.firstBillingDate != null && payData.firstBillingDate!!.dayOfMonth != candidateExp.dayOfMonth)
+        if(payData.firstBillingDate != null && payData.firstBillingDate!!.dayOfMonth != candidateExp.dayOfMonth) {
+            if(YearMonth.from(candidateExp).lengthOfMonth() < payData.firstBillingDate.dayOfMonth){
+                return candidateExp.withDayOfMonth(
+                    YearMonth.from(candidateExp).lengthOfMonth()
+                )
+            }
             return candidateExp.withDayOfMonth(payData.firstBillingDate.dayOfMonth)
+        }
         return payData.billingDate!!.plusMonths(addendMonths)
     }
 }
